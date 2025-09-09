e317e900b5f8779943f075dd891c9df1546114ea
Daniel Elisandro <dsianturi57@.gmail.com>
2025-09-09 16:05:44 +0700

    init: prismadb

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,17 @@
+// src/prisma/prisma.service.ts
+import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  async onModuleInit() {
+    // This connects to the database as soon as the module is initialized.
+    await this.$connect();
+  }
+
+  async enableShutdownHooks(app: INestApplication) {
+    process.on('beforeExit', async () => {
+      await app.close();
+    });
+  }
+}
